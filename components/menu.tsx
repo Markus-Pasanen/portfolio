79ec96e6bc1324636bbd8f583a2bd507dd62dda8
Markus-Pasanen <markus.petteri.pasanen@gmail.com>
2023-11-07 18:09:27 +0200

    swapping pc

---
+++ components/menu.tsx
@@ -0,0 +1,13 @@
+export default () => {
+    return (
+        <main className="flex min-w-screen flex-row items-center justify-between h-16 px-16 bg-red-500">
+            <div>MP</div>
+            <div className="flex flex-row gap-4">
+            <div>Work</div>
+            <div>About</div>
+            <div>Projects</div>
+            <div>Contact</div>
+            </div>
+    </main>
+)
+}
